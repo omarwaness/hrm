13193ed57806ff2744bd23b71514d40578dd5434
MouaadhW <160032369+MouaadhW@users.noreply.github.com>
2025-03-10 12:22:59 +0100

    aaaa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Employee from "./pages/employee"
 import Dashboard from "./pages/dashboard"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
-// App
+// App component
 function App() {
   return (
     <Router>
