318141c283bb4d1060ee5d42ee53e725b259f612
omarwaness <o5v4w2000@gmail.com>
2025-04-09 13:35:06 +0100

    latest version

--- frontend/src/components/employee/employee-sidebar.jsx
+++ frontend/src/components/employee/employee-sidebar.jsx
@@ -13,10 +13,6 @@ export function EmployeeSidebar({ setActiveComponent, ...props }) {
     <Sidebar className="top-(--header-height) h-[calc(100svh-var(--header-height))]!" {...props}>
       <SidebarHeader></SidebarHeader>
       <SidebarContent className="p-4 flex flex-col gap-2">
-        <Button variant="ghost" className="justify-start" onClick={() => setActiveComponent("Inbox")}>  
-          <Inbox className="mr-2 h-5 w-5" />
-          Inbox
-        </Button>
         <Button variant="ghost" className="justify-start" onClick={() => setActiveComponent("Contract")}>  
           <FileText className="mr-2 h-5 w-5" />
           Contract
