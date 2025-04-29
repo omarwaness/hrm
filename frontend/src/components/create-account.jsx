484c9ebaf4cf6af6b2f2577a5b130edc0c287cfa
MouaadhW <160032369+MouaadhW@users.noreply.github.com>
2025-04-29 13:57:27 +0100

    Few changes

--- frontend/src/components/create-account.jsx
+++ frontend/src/components/create-account.jsx
@@ -86,7 +86,7 @@ export function CreateAccountForm({
         const res = await fetch("http://localhost:5000/api/auth/register", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ firstName, lastName, phoneNumber, role: role || 'Conditate', email, password }),
+            body: JSON.stringify({ firstName, lastName, phoneNumber, role: 'Conditate', email, password }),
             credentials: "include",
         });
 
@@ -170,35 +170,17 @@ export function CreateAccountForm({
                     />
                   </div>
                 </div>
-                <div className="grid md:grid-cols-2 md:gap-6">
-                  <div className="grid gap-3">
-                    <Label htmlFor="phoneNumber">Phone Number</Label>
-                    <Input
-                      id="phoneNumber"
-                      type="number"
-                      placeholder="Phone Number"
-                      value={phoneNumber}
-                      onChange={(e) => setPhoneNumber(e.target.value)}
-                      required
-                    />
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="role">Role</Label>
-                    <Select value={role} onValueChange={handleRoleChange}>
-                      <SelectTrigger className="w-full">
-                        <SelectValue placeholder="Select a Role" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectGroup>
-                          <SelectLabel>Role</SelectLabel>
-                          <SelectItem value="HR">HR</SelectItem>
-                          <SelectItem value="Employee">Employee</SelectItem>
-                          <SelectItem value="Admin">Admin</SelectItem>
-                          <SelectItem value="Conditate">Conditate</SelectItem>
-                        </SelectGroup>
-                      </SelectContent>
-                    </Select>
-                  </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="phoneNumber">Phone Number</Label>
+                  <Input
+                    id="phoneNumber"
+                    type="tel"
+                    className="w-full"
+                    placeholder="Enter your phone number"
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                    required
+                  />
                 </div>
               </div>
 
